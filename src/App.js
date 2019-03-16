17f912aa64b025bcd020e38559e7a258e642f98a
tian <tian_chaiya12@hotmail.com>
2019-03-17 00:31:17 +0700

    FIX set up

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
-import React, {Component} from 'react';
-import {NativeModules, Platform, StyleSheet, Text, View} from 'react-native';
-import {RtcEngine, AgoraView} from 'react-native-agora';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import LogIn from './screens/LogIn'
+import Phone from './screens/Phone'
 
-const {Agora} = NativeModules
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const MainNavigator = createStackNavigator({
+  LogIn: { screen: LogIn },
+  Phone: { screen: Phone },
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+const App = createAppContainer(MainNavigator);
 
-  render() {
-    console.log('Agora from Native Module', Agora)
-    console.log('RtcEngine', RtcEngine)
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App
---
+++ src/screens/Phone.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, NativeModules } from 'react-native'
+// import { RtcEngine, AgoraView } from 'react-native-agora';
+
+// const { Agora } = NativeModules
+
+function Phone() {
+  return (
+    <View>
+      <Text>Hello Phone</Text>
+    </View>
+  )
+}
+
+export default Phone
+
