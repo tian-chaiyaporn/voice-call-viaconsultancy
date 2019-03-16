89fb396e4f8bff15cd15794998bd75ea225a2aac
tian <tian_chaiya12@hotmail.com>
2019-03-16 23:45:56 +0700

    ADD basic pages

---
+++ src/pages/LogIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function LogIn() {
+  return (
+    <View>
+      <Text>hello text</Text>
+    </View>
+  )
+}
+
+export default LogIn
+
---
+++ src/pages/Phone.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Phone() {
+  return (
+    <View>
+      <Text>Hello Phone</Text>
+    </View>
+  )
+}
+
+export default Phone
+
