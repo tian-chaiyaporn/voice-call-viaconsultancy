51466707b12333d09897c1e78f6b752a5a16c0b9
tian <tian_chaiya12@hotmail.com>
2019-03-23 20:16:23 +0700

    Set up redux thunk and axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
-import LogIn from './screens/LogIn'
-import Phone from './screens/Phone'
+import LogIn from './screens/LogIn/LogIn'
+import Phone from './screens/Phone/Phone'
 
 const MainNavigator = createStackNavigator({
   LogIn: { screen: LogIn },
