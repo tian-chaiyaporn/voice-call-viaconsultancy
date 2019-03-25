d1bdd154f3d6c402d15741a84919ecf40c86ef32
tian <tian_chaiya12@hotmail.com>
2019-03-25 22:40:40 +0700

    SET UP Agora basic

--- src/screens/Phone/Phone.js
+++ src/screens/Phone/Phone.js
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   View,
   Text,
   NativeModules,
   SafeAreaView,
-  Button
+  Button,
+  Alert
 } from 'react-native'
 import { RtcEngine } from 'react-native-agora'
 import { connect } from 'react-redux'
 import { logout } from '../LogIn/LogInActions'
 
 const { Agora } = NativeModules
+const { AudioProfileDefault } = Agora
 
 function Phone(props) {
   const { navigate } = props.navigation
+
+  useEffect(() => {
+    const config = { appid: '', audioProfile: AudioProfileDefault }
+    RtcEngine.on('joinChannelSuccess', (data) => {
+      RtcEngine.startPreview()
+    })
+
+    RtcEngine.on('userJoined', (data) => {
+      Alert.alert('Someone has joined this conversation')
+    })
+
+    RtcEngine.init(config)
+
+    return () => {
+      RtcEngine.removeAllListeners()
+      RtcEngine.destroy()
+    }
+  }, [])
+
+  const joinChannel = () => {
+    // const { channelName, uid } = props
+    // RtcEngine.joinChannel(channelName, uid);
+  }
+
+  const leaveChannel = () => {
+    console.log('leave channel')
+    // RtcEngine.leaveChannel()
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
@@ -28,8 +59,8 @@ function Phone(props) {
         </View>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <View style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
-            <View style={{ flex: 1 }}><Button title={'Call'} onPress={() => { console.log('Call') }} /></View>
-            <View style={{ flex: 1 }}><Button title={'End Call'} onPress={() => { console.log('End Call')}} /></View>
+            <View style={{ flex: 1 }}><Button title={'Call'} onPress={() => joinChannel()} /></View>
+            <View style={{ flex: 1 }}><Button title={'End Call'} onPress={() => leaveChannel()} /></View>
           </View>
         </View>
       </View>
